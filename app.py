6c526468366a81fed262335df0019645e759a11e
Nicholas Ramsay <nick@yousponsor.me>
2020-01-02 13:46:15 +1100

    added x and z rotation to the cube

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Nick\'s 3D Engine')
 clock = pygame.time.Clock()
 
+fTheta = 0
+
 # Begin game loop
 running = True
 while running:
@@ -90,20 +92,50 @@ while running:
         [ 0.0,                    0.0,     (-fFar * fNear) / (fFar - fNear), 0.0 ]
     ])
 
+    # rotate cube
+    fTheta += 1.0 * pygame.time.get_ticks()/60000
+
+    matRotZ = Matrix4x4(matrix = [
+        [ math.cos(fTheta * 0.5),   math.sin(fTheta * 0.5), 0.0, 0.0 ],
+        [ - math.sin(fTheta * 0.5), math.cos(fTheta * 0.5), 0.0, 0.0 ],
+        [ 0.0,                      0.0,                    1.0, 0.0 ],
+        [ 0.0,                      0.0,                    0.0, 1.0 ]
+    ])
+    matRotX = Matrix4x4(matrix = [
+        [ 1.0, 0.0,                      0.0,                    0.0 ],
+        [ 0.0, math.cos(fTheta * 0.5),   math.sin(fTheta * 0.5), 0.0 ],
+        [ 0.0, - math.sin(fTheta * 0.5), math.cos(fTheta * 0.5), 0.0 ],
+        [ 0.0, 0.0,                      0.0,                    1.0]
+
+    ])
+    
+
     # Draw triangles 
     for tri in mesh.triangles:
         
-        triProjected, triTranslated = Triangle(), tri
+        # rotate the triangles
+        triRotatedZ = Triangle(vectors = [
+            Matrix4x4.MultipleMatrixVector(tri.vectors[0], matRotZ),
+            Matrix4x4.MultipleMatrixVector(tri.vectors[1], matRotZ),
+            Matrix4x4.MultipleMatrixVector(tri.vectors[2], matRotZ)
+        ])
+        triRotatedZX = Triangle(vectors = [
+            Matrix4x4.MultipleMatrixVector(triRotatedZ.vectors[0], matRotX),
+            Matrix4x4.MultipleMatrixVector(triRotatedZ.vectors[1], matRotX),
+            Matrix4x4.MultipleMatrixVector(triRotatedZ.vectors[2], matRotX)
+        ])
 
         # translate the triangles away from "player head"
-        triTranslated.vectors[0].z = tri.vectors[0].z + 3.0
-        triTranslated.vectors[1].z = tri.vectors[1].z + 3.0
-        triTranslated.vectors[2].z = tri.vectors[2].z + 3.0
+        triTranslated = triRotatedZX
+        triTranslated.vectors[0].z = triRotatedZX.vectors[0].z + 3.0
+        triTranslated.vectors[1].z = triRotatedZX.vectors[1].z + 3.0
+        triTranslated.vectors[2].z = triRotatedZX.vectors[2].z + 3.0
 
         # projection, normalise
-        triProjected.vectors[0] = Matrix4x4.MultipleMatrixVector(tri.vectors[0], mat4x4)
-        triProjected.vectors[1] = Matrix4x4.MultipleMatrixVector(tri.vectors[1], mat4x4)
-        triProjected.vectors[2] = Matrix4x4.MultipleMatrixVector(tri.vectors[2], mat4x4)
+        triProjected = Triangle()
+        triProjected.vectors[0] = Matrix4x4.MultipleMatrixVector(triTranslated.vectors[0], mat4x4)
+        triProjected.vectors[1] = Matrix4x4.MultipleMatrixVector(triTranslated.vectors[1], mat4x4)
+        triProjected.vectors[2] = Matrix4x4.MultipleMatrixVector(triTranslated.vectors[2], mat4x4)
 
         # scale back into width & height
         triProjected.vectors[0].x += 1.0; triProjected.vectors[0].y += 1.0
